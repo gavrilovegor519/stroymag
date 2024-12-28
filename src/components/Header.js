36277178f1d18be27bf9ae5f9daba37903c7f423
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2024-12-28 10:36:49 +0300

    Добавил возможность возврата на главную страницу

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import './Header.css'
 import logo from '../media/lotop-dlya-stroitelnoy-kompanii-1024x576.jpg'
+import { useNavigate } from 'react-router-dom';
 
 function Header() {
+    const navigate = useNavigate();
+
     return(
         <div className='Header'>
-            <img src={logo} alt='Логотип сайта' height='70px' />
+            <img src={logo} alt='Логотип сайта' height='70px' onClick={navigate('/')} />
         </div>
     )
 }
