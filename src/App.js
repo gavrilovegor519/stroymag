1ddc9011706f25abe02a46974fca505e63c2b4a4
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2024-12-28 11:25:35 +0300

    Часть контента вынес в отдельную страницу "О нас"

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Main from './views/Main';
 import Services from './views/Services';
+import About from './views/About';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Main />} />
           <Route path='/services' element={<Services />} />
+          <Route path='/about' element={<About />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -8,6 +8,7 @@ function Header() {
             <Link to='/'><img src={logo} alt='Логотип сайта' height='70px'/></Link>
             <ul>
                 <li><Link to='/services' className='links'>Услуги</Link></li>
+                <li><Link to='/about' className='links'>О нас</Link></li>
             </ul>
         </div>
     )
